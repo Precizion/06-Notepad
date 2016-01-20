e02eec4ae6036dd373c6e5ea6e0ff8b0b2a37c4a
Michael Murray <Michael Murray>
2016-01-19 17:32:03 -0800

    Initial Commit

---
+++ Notepad/Notepad/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.FileName = "Document";
+            openfile.DefaultExt = ".txt";
+            openfile.Filter = "Text documents (.txt)|*.txt";
+            Nullable<bool> result = openfile.ShowDialog();
+            if (result == true)
+            {
+                string filename = openfile.FileName;
+                textBox.Text = File.ReadAllText(filename);
+            }
+        }
+
+        private void mnuSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.DefaultExt = ".txt";
+            saveFileDialog1.RestoreDirectory = true;
+            Nullable<bool> result = saveFileDialog1.ShowDialog();
+            if (result == true)
+            {
+                string filename = saveFileDialog1.FileName;
+                //textBox.Text = File.WriteAllText(textBox.Text, filename);
+                File.WriteAllText(filename, textBox.Text);
+            }
+
+        }
+
+        private void mnuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+    }
+}
